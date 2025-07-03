d51bac0ca93355f3fa2aae45a336681894a52552
ItsmeJ0 <jhonatanl.tok@gmail.com>
2025-07-03 18:38:57 +0700

    coba fix data chart

--- app/Http/Controllers/HasilTestController.php
+++ app/Http/Controllers/HasilTestController.php
@@ -126,7 +126,6 @@ class HasilTestController extends Controller
                     ],
                     'options' => ['scale' => ['ticks' => ['beginAtZero' => true]]]
                 ]));
-                dd($dataChart);
                 $imageData = file_get_contents($chartUrl);
                 $chartImage  = base64_encode($imageData);
                 
